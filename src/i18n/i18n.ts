e266f71cee297519bbca6948957799b8f440162d
BTC Tap <249306367+btctap@users.noreply.github.com>
2025-12-20 21:25:41 +0100

    Fix translations

--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -2,10 +2,10 @@ const dict = {
   en: {
     language: "English",
     headline: "Free Bitcoin",
-    subline: "Just for trying it out",
+    subline: "So that you can try it",
     description:
       "Congratulations on finding BTC Tap! We give away real satoshis so that you can learn using them as money within your community. A free web-based wallet will be created for you, just bookmark it in your browser. All we want in return is that you ask to pay with it in your favorite cafes, restaurants and shops. If an owner does not have a wallet, offer to send her a 'gift fund'. This will create a wallet the same way we now do it for you.",
-    continue: "Please continue",
+    continue: "Continue",
     api_offline_msg:
       "Could not connect to the wallet's API, please try again later",
     not_found: "404 - Page Not Found",
@@ -25,10 +25,10 @@ const dict = {
   de: {
     language: "Deutsch",
     headline: "Kostenloser Bitcoin",
-    subline: "Nur um es auszuprobieren!",
+    subline: "Damit Sie es ausprobieren können",
     description:
       "Herzlichen Glückwunsch, dass Sie BTC Tap gefunden haben! Wir verschenken echte Satoshis, damit Sie lernen, sie in Ihrer Gemeinschaft als Geld zu verwenden. Eine kostenlose webbasierte Geldbörse wird für Sie erstellt; speichern Sie sie einfach in Ihrem Browser. Alles, was wir im Gegenzug möchten, ist, dass Sie in Ihren Lieblingscafés, Restaurants und Geschäften darum bitten, damit zu bezahlen. Wenn ein Besitzer keine Geldbörse hat, bieten Sie an, ihr einen „Geschenkfonds“ zu senden. Dadurch wird eine Geldbörse auf die gleiche Weise erstellt, wie wir es jetzt für Sie tun.",
-    continue: "Bitte fahren Sie fort",
+    continue: "Fortfahren",
     api_offline_msg:
       "Es konnte keine Verbindung zur API der Wallet hergestellt werden. Bitte versuchen Sie es später erneut.",
     not_found: "404 - Seite nicht gefunden",
@@ -49,10 +49,10 @@ const dict = {
   es: {
     language: "Español",
     headline: "Bitcoin gratis",
-    subline: "Para que puedas probar",
+    subline: "Para que puedas probarlo",
     description:
       "¡Felicitaciones por encontrar BTC Tap! Regalamos satoshis reales para que puedas aprender a usarlos como dinero dentro de tu comunidad. Se creará una cartera web gratuita para ti; solo guárdala en tu navegador. Lo único que pedimos a cambio es que pidas pagar con ella en tus cafeterías, restaurantes y tiendas favoritas. Si un dueño no tiene una cartera, ofrécele enviarle un 'fondo de regalo'. Esto creará una cartera de la misma manera que lo estamos haciendo ahora por ti.",
-    continue: "Por favor, continúe",
+    continue: "Continuar",
     api_offline_msg:
       "No se pudo conectar a la API de la billetera, inténtelo nuevamente más tarde",
     not_found: "404 - Página no encontrada",
@@ -72,10 +72,10 @@ const dict = {
   pt: {
     language: "Português",
     headline: "Bitcoin grátis",
-    subline: "Apenas para experimentá-lo",
+    subline: "Para que você possa experimentá-lo",
     description:
       "Parabéns por encontrar o BTC Tap! Nós damos satoshis reais para que você possa aprender a usá-los como dinheiro dentro da sua comunidade. Uma carteira baseada na web será criada para você; basta adicioná-la aos favoritos do seu navegador. Tudo o que queremos em troca é que você peça para pagar com ela em seus cafés, restaurantes e lojas favoritos. Se um proprietário não tiver uma carteira, ofereça-se para enviar um 'fundo de presente'. Isso criará uma carteira da mesma forma que agora estamos fazendo por você.",
-    continue: "Por favor, continue",
+    continue: "Continuar",
     api_offline_msg:
       "Não foi possível conectar à API da carteira. Tente novamente mais tarde.",
     not_found: "404 - Página não encontrada",
@@ -95,10 +95,10 @@ const dict = {
   zh: {
     language: "中文",
     headline: "免费比特币",
-    subline: "只是为了试试看",
+    subline: "以便你可以尝试",
     description:
       "祝贺你找到BTC Tap！我们赠送真实的Satoshi，以便你能在自己的社区中学习如何用它们作为货币。我们将为你创建一个免费的网络钱包，只需将其添加到浏览器书签中。我们希望的唯一回报是，你能够在自己喜欢的咖啡馆、餐厅和商店里要求用它付款。如果某位店主没有钱包，请提议给她发送一个“礼品基金”。这样会以我们现在为你创建钱包的方式来创建一个钱包。",
-    continue: "请继续",
+    continue: "继续",
     api_offline_msg: "无法连接到钱包的 API，请稍后重试。",
     not_found: "404 - 页面未找到",
     not_found_subline: "您要查找的页面不存在。",
@@ -117,10 +117,10 @@ const dict = {
   ja: {
     language: "日本語",
     headline: "無料のビットコイン",
-    subline: "試してみるためだけに",
+    subline: "試せるように",
     description:
       "BTC Tapを見つけたことをお祝いします！私たちは、あなたがコミュニティ内でお金として使う方法を学ぶために本物のサトシを配布します。あなたのために無料のウェブベースの財布が作成され、ブラウザにブックマークするだけです。私たちが求めているのは、お気に入りのカフェ、レストラン、店舗でそれを使って支払うように頼むことだけです。オーナーが財布を持っていない場合は、彼女に「ギフトファンド」を送ることを提案してください。これにより、今私たちがあなたのために行っているのと同じ方法で財布が作成されます。",
-    continue: "続けてください",
+    continue: "続ける",
     api_offline_msg:
       "ウォレットのAPIに接続できませんでした。しばらくしてからもう一度お試しください。",
     not_found: "404 - ページが見つかりません",
@@ -143,7 +143,7 @@ const dict = {
     subline: "Просто, чтобы вы попробовали",
     description:
       "Поздравляем с тем, что вы нашли BTC Tap! Мы раздаем настоящие сатоши, чтобы вы могли научиться использовать их как деньги в своем сообществе. Для вас будет создан бесплатный веб-кошелек, просто добавьте его в закладки в вашем браузере. Все, что мы хотим взамен, — это чтобы вы просили расплатиться им в своих любимых кафе, ресторанах и магазинах. Если у владельца нет кошелька, предложите отправить ей «подарочный фонд». Это создаст кошелек таким же образом, как мы сейчас делаем это для вас.",
-    continue: "Прошу продолжить",
+    continue: "Продолжить",
     api_offline_msg:
       "Не удалось подключиться к API кошелька. Пожалуйста, попробуйте позже.",
     not_found: "404 - Страница не найдена",
