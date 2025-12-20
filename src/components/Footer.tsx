dc8825530b51e0fb01b345c6f9fcd516ad5b5163
BTC Tap <249306367+btctap@users.noreply.github.com>
2025-12-20 19:45:46 +0100

    Add repo url

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,18 @@
 import { config } from "../config";
 import { useGlobalContext } from "../context/Global";
 import "../style/footer.scss";
+import { BsGithub } from "solid-icons/bs";
 
 const Footer = () => {
   const { t } = useGlobalContext();
 
   return (
     <footer>
+      <div class="socials">
+        <a title="Github" class="github" target="_blank" href={config.repoUrl}>
+          <BsGithub size={22} color="#22374F" />
+        </a>
+      </div>
       <p class="version">
         {t("commithash")}:{" "}
         <a target="_blank" href={`${config.repoUrl}/commit/${__GIT_COMMIT__}`}>
