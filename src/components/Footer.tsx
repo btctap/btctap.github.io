54011fdeeba13201fe01f1570dcce8504417e757
BTC Tap <249306367+btctap@users.noreply.github.com>
2025-12-21 19:17:08 +0100

    Fix footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,20 +1,18 @@
 import { config } from "../config";
 import { useGlobalContext } from "../context/Global";
 import "../style/footer.scss";
-import { BsGithub } from "solid-icons/bs";
 
 const Footer = () => {
   const { t } = useGlobalContext();
 
   return (
     <footer>
-      <div class="socials">
-        <a title="Github" class="github" target="_blank" href={config.repoUrl}>
-          <BsGithub size={22} color="#22374F" />
-        </a>
-      </div>
       <p class="version">
-        {t("commithash")}:{" "}
+        {t("version")}:{" "}
+        <a target="_blank" href={`${config.repoUrl}`}>
+          {__APP_VERSION__}
+        </a>
+        , {t("commithash")}:{" "}
         <a target="_blank" href={`${config.repoUrl}/commit/${__GIT_COMMIT__}`}>
           {__GIT_COMMIT__}
         </a>
